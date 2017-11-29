34e7b7756e98672661adfbe067faad7d2cb43d56
Jdville03 <jarrel.delottinville@gmail.com>
2017-11-28 23:30:11 -0800

    Done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ToggleButton from './components/ToggleButton';
 import Address from './components/Address';
 
 ReactDOM.render(
-  <Address
-    street="Santa Monica Blvd."
-    city="Santa Monica"
-  />,
+  <div>
+    <ToggleButton /><br /><br />
+    <Address
+      street="Santa Monica Blvd."
+      city="Santa Monica"
+    />
+  </div>,
   document.getElementById('root')
 );
